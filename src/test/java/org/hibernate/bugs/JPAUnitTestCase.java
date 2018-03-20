9851f57c6fbd680be20b7f7df860f334258c5cad
Giovanni Lovato <giovanni.lovato@axians.it>
2018-03-20 15:36:29 +0100

    Update to 5.2.15

--- src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,10 +1,14 @@
 package org.hibernate.bugs;
 
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class JPAUnitTestCase {
 
@@ -12,12 +16,21 @@ public class JPAUnitTestCase {
 
     @Before
     public void init() {
-        this.entityManagerFactory = Persistence.createEntityManagerFactory("h2");
+        this.entityManagerFactory = Persistence
+                .createEntityManagerFactory("h2");
     }
 
     @After
     public void destroy() {
         this.entityManagerFactory.close();
+    }
+
+    @Test
+    public void testTemplate() throws SQLException {
+        EntityManager em = this.entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+        // test code goes here
+        em.getTransaction().commit();
     }
 
 }
