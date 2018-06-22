5273964dfe9de123bbbb3e63d978a4a81e931acd
Giovanni Lovato <giovanni.lovato@axians.it>
2018-06-22 11:10:36 +0200

    HHH-3930

--- src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -2,9 +2,13 @@ package org.hibernate.bugs;
 
 import java.sql.SQLException;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,6 +22,20 @@ public class JPAUnitTestCase {
     public void init() {
         this.entityManagerFactory = Persistence
                 .createEntityManagerFactory("h2");
+        EntityManager em = this.entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        Foo foo = new Foo();
+        foo.id = "foo";
+        foo.bar = new Bar();
+        foo.bar.id = "bar";
+        foo.bar.name = "bar";
+
+        em.persist(foo.bar);
+        em.persist(foo);
+
+        em.getTransaction().commit();
+        em.close();
     }
 
     @After
@@ -28,9 +46,34 @@ public class JPAUnitTestCase {
     @Test
     public void testTemplate() throws SQLException {
         EntityManager em = this.entityManagerFactory.createEntityManager();
-        em.getTransaction().begin();
-        // test code goes here
-        em.getTransaction().commit();
+        CriteriaQuery<Foo> criteriaQuery = em.getCriteriaBuilder()
+                .createQuery(Foo.class);
+        criteriaQuery.from(Foo.class);
+        em.createQuery(criteriaQuery).getResultList();
     }
+
+}
+
+@Entity
+class Foo {
+
+    @Id
+    String id;
+
+    @OneToOne
+    Bar bar;
+
+}
+
+@Entity
+class Bar {
+
+    @Id
+    String id;
+
+    String name;
+
+    @OneToOne(mappedBy = "bar")
+    Foo foo;
 
 }
